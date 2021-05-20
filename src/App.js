20bf81bb8989d8bc2f3129244ebfdb02a2241e53
Doingitgud <www.acer2linkblink@gmail.com>
2021-05-20 12:33:21 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import jsxIntro from './components/jsx_hello'
+
 function App() {
   return (
     <div className="App">
--- src/components/jsx_hello.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const introJSX = () =>{
-    return(
-        <h1>Hello Hassan</h1>
-    )
-}
-export default introJSX
