bbb935277d9addb7102c37c4ba022b7bc926c9ec
Doingitgud <www.acer2linkblink@gmail.com>
2021-05-20 13:49:42 +0600

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+//import React from 'react';
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+/*
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    )
+  }
+}
+*/
+
+//we're loading the Component as a property of React, so we no longer need to extend React.Component.
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    )
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'))
